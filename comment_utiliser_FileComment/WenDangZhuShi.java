273e4ab578812c53b6ad35a3c6a097611ba5bbe9
Yufei Li <102528867+Lik99@users.noreply.github.com>
2023-09-21 22:46:48 +0200

    Add files via upload

---
+++ comment_utiliser_FileComment/WenDangZhuShi.java
@@ -0,0 +1,15 @@
+/**
+ * @author LiYufei
+ * @version 1.0
+ */
+
+ /* 如何应用文档注释？
+  使用代码：javadoc -d 要把生成的网页注释放到某一文件夹的地址 -注释1 -注释2 使用网页注释的源码文件名
+  例如： javadoc -d /Users/Yveine/Java -author -version WenDangZhuShi.java
+  */
+
+public class WenDangZhuShi {
+    public static void main (String[] args) {
+
+    }
+}
